644d157f344f7674dc17b48b664330b16732c858
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2026-04-03 12:05:59 -0500

    Update frog-game.js

--- frog-game.js
+++ frog-game.js
@@ -616,7 +616,7 @@ const survivalIds = [
   let nextPermanentChoiceTime = 60;
 
   // every 180 seconds we pause for an EPIC upgrade
-  let nextEpicChoiceTime = 30;
+  let nextEpicChoiceTime = 180;
 
   let legendaryEventTriggered = false;
 
@@ -5880,7 +5880,7 @@ function getDashboardPfp() {
       if (currentUpgradeOverlayMode === "normal") {
         nextPermanentChoiceTime = elapsedTime + 60;
       } else if (currentUpgradeOverlayMode === "epic") {
-        nextEpicChoiceTime = elapsedTime + 30;
+        nextEpicChoiceTime = elapsedTime + 180;
       }
     }
   }
@@ -6045,7 +6045,7 @@ function getDashboardPfp() {
     // Reset upgrade timing / sheds
     initialUpgradeDone       = false;
     nextPermanentChoiceTime  = 60;
-    nextEpicChoiceTime       = 30;
+    nextEpicChoiceTime       = 180;
     legendaryEventTriggered  = false;
     orbSpecialistActive      = false; 
 
