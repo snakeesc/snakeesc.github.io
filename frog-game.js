fb8c7729cc7f48205abef88ae816f8848dfa99e6
NF7UOS <97641869+freshfrogs@users.noreply.github.com>
2026-04-07 18:58:24 -0500

    Update frog-game.js

--- frog-game.js
+++ frog-game.js
@@ -1115,7 +1115,7 @@ const SNAKE_SKIN_STORAGE_KEY = "frogSnake_selectedSnakeSkin";
       head: "./images/head3.png",
       body: "./images/body3.png",
       tail: "./images/tail3.png",
-      requiredLevel: 10
+      requiredLevel: 15
     }
   ];
 
